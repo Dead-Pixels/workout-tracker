b0e38fab7f5c65bc7675937c42aa601fea9710c8
mundeeppurewal <mundeep.purewal@gmail.com>
2019-05-29 13:17:29 +0100

    commiting project structure

---
+++ workout-tracker-mvc/src/main/java/com/sunnypurewal/workouttrackermvc/WorkoutTrackerMvcApplication.java
@@ -0,0 +1,13 @@
+package com.sunnypurewal.workouttrackermvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutTrackerMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutTrackerMvcApplication.class, args);
+	}
+
+}
---
+++ workout-tracker-mvc/src/test/java/com/sunnypurewal/workouttrackermvc/WorkoutTrackerMvcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sunnypurewal.workouttrackermvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WorkoutTrackerMvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
